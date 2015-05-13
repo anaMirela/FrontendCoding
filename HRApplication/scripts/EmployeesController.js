7feaeccc2d6f796a18f44a2b5998d44fbd70db11
anaMirela <ana.mirela.irimia@gmail.com>
2015-05-13 12:31:20 +0300

    First commit final app

---
+++ HRApplication/scripts/EmployeesController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Mi on 5/12/2015.
+ */
+hrApp.controller('EmployeesController', ['$scope', function($scope){
+    $scope.employeesList = ['Mirela', 'Irimia', 'Ana'];
+}]);
---
+++ HRApplication/scripts/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Mi on 5/12/2015.
+ */
+var hrApp = angular.module('hrApp', []);
